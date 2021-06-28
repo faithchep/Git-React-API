36dce221cca922f7ccd1289dd7e1a04496edf7ac
faithchep <faithchepkoech40@gmail.com>
2021-06-28 16:13:12 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+//react-router
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+//toast
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+
+//firebase
+import firebase from "firebase/app";
+import "firebase/auth";
+
+//components
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Pagenotfound from "./pages/Pagenotfound";
+import { UserContext } from './Context/UserContext';
+import Footer from './layout/footer';
+import Header from './layout/header';
+
+import firebaseConfig from "./Config/FirebaseConfig";
+//init firebase
+firebase.initializeApp(firebaseConfig);
+
+const App = () => {
+
+  const [user, setUser] = useState(null);
+
+  return (
+    <Router>
+      <ToastContainer />
+      <UserContext.Provider value={{user, setUser}}>
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="*" component={Pagenotfound} />
+        </Switch>
+        <Footer/>
+      </UserContext.Provider>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Config/FirebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyC43Skg_7N5edg3aJeSDpzr_fZ7B-OkP7A",
+    authDomain: "mygitapp-983c5.firebaseapp.com",
+    databaseURL: "https://mygitapp-983c5.firebaseio.com",
+    projectId: "mygitapp-983c5",
+    storageBucket: "mygitapp-983c5.appspot.com",
+    messagingSenderId: "615522962492",
+    appId: "1:615522962492:web:6d8d03ea08e97cf0ecc739",
+    measurementId: "G-8KZQQSDS0V"
+};
